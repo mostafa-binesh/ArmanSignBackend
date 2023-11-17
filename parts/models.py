b01548523f1f7a6b1999ec92fdf4a1f031aba2a6
Mostafa <ubirockteam@gmail.com>
2023-11-17 14:26:51 +0330

    feat: add more transaltions

--- parts/models.py
+++ parts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Part(models.Model):
-    title = models.CharField(max_length=200)
-    created_at = models.DateTimeField("created_at", auto_now_add=True)
+    title = models.CharField(max_length=200,verbose_name=_("Title"))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
+
+    class Meta:
+        verbose_name = _("Part")
+        verbose_name_plural = _("Parts")
 
     def __str__(self) -> str:
         return self.title
