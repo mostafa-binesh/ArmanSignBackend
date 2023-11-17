561e61fc9a41b4d69e68475943b5698fcf594f92
Mostafa <ubirockteam@gmail.com>
2023-11-17 15:56:07 +0330

    fix: alter created at field in order model

--- orders/models.py
+++ orders/models.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from parts.models import Part
 from clients.models import Client
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 # Create your models here.
 class Order(models.Model):
     parts = models.ManyToManyField(Part, verbose_name=_("Parts"))
     client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name=_("Client"))
     count = models.IntegerField(default=0, verbose_name=_("Count"))
-    created_at = models.DateTimeField(default=datetime.now(), verbose_name=_("Created At"))
+    created_at = models.DateTimeField(default=timezone.now, verbose_name=_("Created At"))
     order_number = models.CharField(max_length=200, verbose_name=_("Order Number"))
     
     class Meta:
