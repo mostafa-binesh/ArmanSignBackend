48af0c1fde1f6cb6a1d3f4f2aeeb7409e264e6dc
Mostafa <ubirockteam@gmail.com>
2023-12-08 17:59:49 +0330

    feat: revert the pagination, if all_data param was exist in the query, return all dat

--- inventory/pagination.py
+++ inventory/pagination.py
@@ -1,19 +1,37 @@
+# from rest_framework.pagination import PageNumberPagination
+# from rest_framework.response import Response
+
+# class ConditionalPagination(PageNumberPagination):
+#     def paginate_queryset(self, queryset, request, view=None):
+#         # Check if 'page' or 'page_size' query parameters are provided
+#         page_size_query_param = self.page_size_query_param
+#         page_query_param = self.page_query_param
+
+#         if page_query_param in request.query_params or page_size_query_param in request.query_params:
+#             # Apply pagination with the provided query parameter
+#             return super().paginate_queryset(queryset, request, view)
+#         else:
+#             # Return the entire queryset for non-paginated response
+#             return queryset
+
+#     def get_paginated_response(self, data):
+#         # Return non-paginated response wrapped in 'results' key
+#         return Response({'results': data})
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 class ConditionalPagination(PageNumberPagination):
     def paginate_queryset(self, queryset, request, view=None):
-        # Check if 'page' or 'page_size' query parameters are provided
-        page_size_query_param = self.page_size_query_param
-        page_query_param = self.page_query_param
-
-        if page_query_param in request.query_params or page_size_query_param in request.query_params:
-            # Apply pagination with the provided query parameter
-            return super().paginate_queryset(queryset, request, view)
-        else:
-            # Return the entire queryset for non-paginated response
-            return queryset
+        # Check for a 'all_data' query parameter
+        if 'all_data' in request.query_params:
+            return None  # Return None to bypass pagination
+        # If 'all_data' is not present, proceed with default pagination
+        return super().paginate_queryset(queryset, request, view)
 
     def get_paginated_response(self, data):
-        # Return non-paginated response wrapped in 'results' key
-        return Response({'results': data})
+        if hasattr(self, 'page'):
+            # If pagination was applied, return the normal paginated response
+            return super().get_paginated_response(data)
+        else:
+            # If pagination was bypassed, wrap the data in 'results' key
+            return Response({'results': data})
