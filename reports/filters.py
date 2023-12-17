974abd3d614cad77aa21262408718bd4cf6c7ccc
Mostafa <ubirockteam@gmail.com>
2023-12-17 12:32:11 +0330

    add report filters

--- reports/filters.py
+++ reports/filters.py
@@ -1,12 +1,21 @@
 from django.forms import DateField
 import django_filters
 from django_filters.rest_framework import DateFilter
+
+from machines.models import Machine
 from .models import Report
 
 class ReportFilter(django_filters.FilterSet):
     # todo
     started_at = DateFilter(field_name="started_at", lookup_expr=('gt'),)
     ended_at = DateFilter(field_name="ended_at", lookup_expr=('gt'),)
+
+    # machine__name = django_filters.CharFilter(lookup_expr='icontains')
+    machine = django_filters.ModelChoiceFilter(queryset=Machine.objects.all())
+    status = django_filters.ChoiceFilter(choices=Report.STATUS_CHOICES)
+
+
+
 
     class Meta:
         model = Report
