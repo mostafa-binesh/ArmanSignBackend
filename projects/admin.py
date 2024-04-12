16a5c89bdaa24d33d4c1c961cf220ac63099b1d4
mostafa-binesh <ubirockteam@gmail.com>
2024-04-12 13:19:40 +0330

    new admin panel

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
 from .models import Project
-# Register your models here.
-admin.site.register(Project)
+from django.db.models import F
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'sort_order')
+    actions = ['move_selected_up', 'move_selected_down']
+
+    def move_selected_up(self, request, queryset):
+        # Move selected items up by decreasing their sort_order
+        for project in queryset:
+            # Make sure we don't go into negative sort orders
+            if project.sort_order > 1:
+                project.sort_order = F('sort_order') - 1
+                project.save()
+        self.message_user(request, "تتیب موارد انتخاب شده به بالا شیفت پیدا کردند.")
+    move_selected_up.short_description = "شیفت ترتیب به بالا"
+
+    def move_selected_down(self, request, queryset):
+        # Move selected items down by increasing their sort_order
+        max_sort_order_item = Project.objects.all().order_by('-sort_order').first()
+        max_sort_order = Project.objects.all().order_by('-sort_order').first().sort_order
+        for project in queryset:
+            # Prevent going beyond the maximum sort order
+            if project.sort_order <= max_sort_order and project.id != max_sort_order_item.id:
+                project.sort_order = F('sort_order') + 1
+                project.save()
+        self.message_user(request, "ترتیب موارد انتخاب شده به پایین شیفت داده شدند.")
+    move_selected_down.short_description = "شیفت ترتیب به پایین"
--- projects/models.py
+++ projects/models.py
@@ -3,6 +3,8 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200,verbose_name=_("Title"))
+    sort_order = models.PositiveIntegerField(default=1, blank=False, null=False,verbose_name=_("Sort Order"))
+    
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
     updated_at = models.DateTimeField(auto_now=True)
 
