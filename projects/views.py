3ff2403c0f476da0eed5f34ed7286d78c4b849ac
Mostafa <ubirockteam@gmail.com>
2024-04-22 13:01:19 +0330

    Update views.py
    sort order

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,6 @@ from .serializers import ProjectSerializer
 from .models import Project
 from rest_framework import viewsets
 class ProjectViewSet(viewsets.ModelViewSet):
-    queryset = Project.objects.all()
+    queryset = Project.objects.all().order_by('sort_order')  # Explicitly ordering by 'sort_order'
     serializer_class = ProjectSerializer
     # permission_classes = [IsAdminUser]
